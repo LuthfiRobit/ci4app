b3009e7e98b6176900f9eacf822b0f7f8415a1c0
LuthfiRobit <luthfirobit@gmail.com>
2021-04-09 22:14:07 +0700

    add fillable variable and add method

--- app/Models/KomikModel.php
+++ app/Models/KomikModel.php
@@ -3,12 +3,14 @@
 namespace App\Models;
 
 use CodeIgniter\Model;
+use db;
 
 class KomikModel extends Model
 {
     protected $table = 'tb_komik';
-    protected $primaryKey = 'id_komik';
     protected $useTimestamps = true;
+    protected $allowedFields = ['judul', 'slug', 'penulis', 'penerbit', 'sampul'];
+
 
     public function getKomik($slug = false){
         if ($slug == false) {
@@ -16,5 +18,10 @@ class KomikModel extends Model
         }
 
         return $this->where(['slug' => $slug])->first();
+    }
+
+    public function add($data){
+        //$this->db->table('tb_komik')->insert($data);
+        $this->db->table('tb_komik')->insert($data);
     }
 }
