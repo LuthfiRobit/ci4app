714431f2b00c05907d21eef8672ef29702536a5a
LuthfiRobit <luthfirobit@gmail.com>
2021-04-09 22:14:39 +0700

    add session alert

--- app/Views/komik/index.php
+++ app/Views/komik/index.php
@@ -3,7 +3,16 @@
 <div class="container">
     <div class="row">
         <div class="col">
+            <a href="/Komik/create" class="btn btn-primary mt-2">Tambah Komik</a>
             <h1 class="mt-2">Daftar Komik</h1>
+            <?php if(session()->getFlashdata('pesan')) :?>
+            <div class="alert alert-primary alert-dismissible fade show" role="alert">
+                <?= session()->getFlashData('pesan');?>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <?php endif;?>
             <table class="table">
                 <thead>
                     <tr>
